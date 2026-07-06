6d40d9a0fccaf02157764285830468ab518a0ee8
BacigalupoSofia <bacigalupo.ie@gmail.com>
2026-07-06 23:03:25 +0100

    Java Script event listener for click in buttons to navigate

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Java script working");
+
+// INDEX HTML
+
+let exploreButton = document.querySelector(".explore.button");
+let addButton = document.querySelector(".add.button");
+let favoritesButton = document.querySelector(".favorites.button");
+
+function navigate(route) {
+  window.location.href = route;
+}
+
+exploreButton.addEventListener("click", () => navigate("recipe_list.html"));
+addButton.addEventListener("click", () => navigate("add_recipe.html"));
+favoritesButton.addEventListener("click", () => navigate("favorites.html"));
+
+// CREAR ADD Y FAVORITOS 
